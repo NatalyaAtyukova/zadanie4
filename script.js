c78099e8c2fc901c81f5740943e52e67b5cae018
NatalyaAtyukova <85542306+NatalyaAtyukova@users.noreply.github.com>
2022-05-31 18:43:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.querySelector('.themetoggle').addEventListener('click', (event) => {
+  event.preventDefault();
+  if (localStorage.getItem('theme') === 'dark') {
+    localStorage.removeItem('theme');
+  }
+  else {
+    localStorage.setItem('theme', 'dark')
+  }
+  addDarkClassToHTML()
+});
+
+function addDarkClassToHTML() {
+  try {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.querySelector('html').classList.add('dark');
+      document.querySelector('.themetoggle span').textContent = 'dark_mode';
+    }
+    else {
+      document.querySelector('html').classList.remove('dark');
+      document.querySelector('.themetoggle span').textContent = 'wb_sunny';
+    }
+  } catch (err) { }
+}
+
+addDarkClassToHTML();
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
